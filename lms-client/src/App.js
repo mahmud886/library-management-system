d3c091a22a7656a9815c35dbb45d61f6fb594ed5
mahmud886 <iqbal886mahmud@gmail.com>
2022-01-08 01:21:02 +0600

    navbar and login pages done

--- lms-client/src/App.js
+++ lms-client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import SignIn from './components/Auth/SignIn';
+import SignUp from './components/Auth/SignUp';
+import Navbar from './components/Navbar/Navbar';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App banner'>
+            <Router>
+                <Navbar />
+                <Switch>
+                    <Route exact path='/' component={SignIn} />
+                    <Route path='/sign-in' component={SignIn} />
+                    <Route path='/sign-up' component={SignUp} />
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
